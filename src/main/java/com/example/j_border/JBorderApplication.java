f4202c893d2b99dda9cf80f8bb22ed4406f4753d
LENOVO <siyoung051026@gmail.com>
2023-03-14 12:53:20 +0900

    Main

---
+++ src/main/java/com/example/j_border/JBorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.j_border;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/j_border/JBorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j_border;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
